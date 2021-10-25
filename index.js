eed68f44b7c076ffbe7457017d731a16657ec723
Aaron Sando <Aaron@Charactercabinets.com.au>
2021-10-25 13:34:54 +0800

    updates after tutor session

--- index.js
+++ index.js
@@ -1,86 +1,147 @@
-// TODO: Include packages needed for this application
-const fs = require("fs");
 const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMd = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-function promptUser(){
-  return inquirer.prompt([
-    {
-      type: "input",
-      message: "What is the project title?",
-      name: "projectTitle",
-    },
-    {
-      type: "input",
-      message: "Write a brief description of your project: ",
-      name: "description",
-    },
-    {
-      type: "input",
-      message: "Describe the installation process if any: ",
-      name: "installation",
-    },
-    {
-      type: "input",
-      message: "What is this project usage for?",
-      name: "usage",
-    },
-    {
-      type: "list",
-      message: "Chose the appropriate license for this project: ",
-      choices: [
-        "Apache",
-        "Academic",
-        "GNU",
-        "ISC",
-        "MIT",
-        "Mozilla",
-        "Open"
-      ],
-      name: "license",
-    },
-    {
-      type: "input",
-      message: "Who are the contributors of this projects?",
-      name: "contributing",
-    },
-    {
-      type: "input",
-      message: "Is there a test included?",
-      name: "tests",
-    },
-    {
-      type: "input",
-      message: "What do I do if I have an issue? ",
-      name: "questions",
-    },
-    {
-      type: "input",
-      message: "Please enter your GitHub username: ",
-      name: "username",
-    },
-    {
-      type: "input",
-      message: "Please enter your email: ",
-      name: "email",
+const writeFileAsync = util.promisify(fs.writeFile);
+
+function makeRequiredFieldValidator(fieldname){
+
+  return function (input, answers) {
+    if (input === "") {
+      return "Please make sure " + fieldname + " has been entered";
+    } else {
+      return true;
     }
-  ]);
-} 
+  }
+}
 
-// TODO: Create a function to write README file
-// function writeToFile(fileName, data) {}
 
-// TODO: Create a function to initialize app
-async function init() {
+const questions = [
+  {
+    type: "input",
+    message: "What is the project title?",
+    name: "projectTitle",
+    validate: makeRequiredFieldValidator("a title"),
+  },
+  {
+    type: "confirm",
+    message: "Would you like a description guide?",
+    name: "needGuide",
+  },  
+  {
+    type: "input",
+    message: "Why did you build this project?",
+    name: "descriptionWhy",
+    validate: makeRequiredFieldValidator("an input"),
+    when: function(answers){
+      return answers.needGuide;
+    }
+  },
+  {
+    type: "input",
+    message: "What problem does it solve?",
+    name: "descriptionWhat",
+    validate: makeRequiredFieldValidator("an input"),
+    when: function(answers){
+      return answers.needGuide;
+    }
+  },
+  {
+    type: "input",
+    message: "What did you learn?",
+    name: "descriptionLearn",
+    validate: makeRequiredFieldValidator("an input"),
+    when: function(answers){
+      return answers.needGuide;
+    }
+  },
+  {
+    type: "input",
+    message: "Please enter a basic description of your project",
+    name: "description",
+    validate: makeRequiredFieldValidator("a description"),
+    when: function(answers){
+      return !answers.needGuide;
+    }
+  },
+  {
+    type: "input",
+    message: "Describe the installation process if any: ",
+    name: "installation",
+    validate: makeRequiredFieldValidator("an installation process"),
+  },
+  {
+    type: "input",
+    message: "What is this project usage for?",
+    validate: makeRequiredFieldValidator("an input"),
+    name: "usage",
+  },
+  {
+    type: "list",
+    message: "Chose the appropriate license for this project: ",
+    choices: [
+      "MIT", 
+      "APACHE 2.0", 
+      "GPL 3.0", 
+      "BSD 3",
+    ],
+    name: "license",
+  },
+  {
+    type: "input",
+    message: "Who are the contributors of this projects?",
+    name: "contribute",
+    validate: makeRequiredFieldValidator("a contributor"),
+  },
+  {
+    type: "confirm",
+    message: "Is there a test included?",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "What do I do if I have an issue? ",
+    name: "questions",
+  },
+  {
+    type: "input",
+    message: "Please enter your GitHub username: ",
+    name: "username",
+  },
+  {
+    type: "input",
+    message: "Please enter your email: ",
+    name: "email",
+  }
+]
+
+// function to prompt user - returns answers object
+const promptUser = () => {
+  return inquirer
+      .prompt(questions);
+}
+
+// function to write README file
+const writeToFile = (fileName, data) => {
+  return writeFileAsync(fileName, data);
+}
+
+const init = async () => {
   try {
-      // Ask user questions and generate responses
-      const answers = await promptUser();
-      const generateContent = generateReadme(answers);
-      // Write new README.md to dist directory
-      await writeFileAsync('./dist/README.md', generateContent);
-      console.log('Successfully wrote to README 👍');
-  }   catch(err) {
-      console.log(err);
+    console.log("Welcome to the README generator.\nPlease answer the following questions:")
+    // ask user for answers to questions
+    const answers = await promptUser();
+    // create markdown content from user answers
+    const fileContent = generateMd(answers);
+    // write markdown content to README.md file
+    await writeToFile("./output/README.md", fileContent);
+    // notify user that file has been written
+    console.log("README.md created in output folder.");
+
+  } catch (err) {
+    console.error("Error creating README. File not created.");
+    console.log(err);
   }
 }
 
---
+++ output/README.md
@@ -0,0 +1,30 @@
+# ged
+![alt text ](https://img.shields.io/badge/License-MIT-yellow)
+
+## Description
+sdg  
+## Table of Contents
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+## Installation
+To install dependencies, run the following:
+`
+sdg
+`
+## Usage
+asdg
+## License
+This repository is licensed under the MIT license.
+## Contributing
+asdg
+## Tests
+To run tests, run the following:
+`
+false
+`
+## Questions
+Questions about this repository? Please contact me at [sadg](mailto:sadg). View more of my work in GitHub at [asdg](https://github.com/asdg) 
